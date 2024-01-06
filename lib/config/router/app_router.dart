cedba404ed0118de3c9f4b9914354d481f67bb15
David Emmanuel Gomez Arca <davegomez426@gmail.com>
2024-01-06 13:20:36 -0300

    go router configurable

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:go_router/go_router.dart';
+import 'package:widget_app/presentation/screens/Screens.dart';
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: "/",
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => ButtonsScreen(),
+    ),
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => CardsScreen(),
+    ),
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/router/app_router.dart';
 import 'package:widget_app/config/theme/app_theme.dart';
-import 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widget_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widget_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: AppTheme(selectedColor: 4).getTheme(),
-      home: const HomeScreen(),
-      routes: {
-        '/buttons': (context) => const ButtonsScreen(),
-        '/cards': (context) => const CardsScreen(),
-      },
     );
   }
 }
