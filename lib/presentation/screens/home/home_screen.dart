222c8a7f84bbdfaa8cd607ae837514a16344ee34
David Emmanuel Gomez Arca <davegomez426@gmail.com>
2024-01-06 12:25:24 -0300

    Home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,52 @@ class HomeScreen extends StatelessWidget {
         title: Text("Flutter + material 3"),
         centerTitle: false,
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    appMenuITems;
+
+    return ListView.builder(
+        itemCount: appMenuITems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuITems[index];
+
+          return _CustomListTile(menuItem: menuItem);
+        });
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        menuItem.icon,
+        color: colors.primary,
+      ),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: colors.primary,
+      ),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {},
     );
   }
 }
